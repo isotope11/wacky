84de59b6507fe8cd30938cd6e95479babe7b788f
amber <amber@amber-DX4831.(none)>
2013-01-17 16:33:11 -0600

    Made SpecialPagesIntegrationSpec pass

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "http://rubygems.org"
 gem 'pg'
 
 gem 'wikitop', github: 'knewter/wikitop'
+gem 'pry'
 #gem 'wikitop', path: '/home/jadams/ruby/wikitop'
 
 gemspec
--- spec/integration/special_pages_integration_spec.rb
+++ spec/integration/special_pages_integration_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 class SpecialPagesIntegrationSpec < AcceptanceSpec
   describe "viewing the recently_updated page" do
     it "can be viewed" do
-      skip
-      Page.create(slug: 'index', body: 'Hoo **boy** [link](/link)')
+      # skip
+      page = Page.create(slug: 'index', body: 'Hoo **boy** [link](/link)')
       visit "/recently_updated"
-      page.must have_text "index"
+      page.slug.must have_text "index"
     end
   end
 end
