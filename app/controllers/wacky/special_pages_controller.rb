559d23083a6d9989a998ab9d154ff95d6223745e
amber <amber@amber-DX4831.(none)>
2013-01-22 14:24:54 -0600

    Make special pages integration spec pass

--- app/controllers/wacky/special_pages_controller.rb
+++ app/controllers/wacky/special_pages_controller.rb
@@ -1,4 +1,5 @@
 class Wacky::SpecialPagesController < ApplicationController
   def recently_updated
+    @pages = Page.order("updated_at DESC").limit(10)
   end
 end
--- spec/integration/special_pages_integration_spec.rb
+++ spec/integration/special_pages_integration_spec.rb
@@ -4,9 +4,9 @@ class SpecialPagesIntegrationSpec < AcceptanceSpec
   describe "viewing the recently_updated page" do
     it "can be viewed" do
       # skip
-      page = Page.create(slug: 'index', body: 'Hoo **boy** [link](/link)')
+      Page.create(slug: 'updated-page', body: 'Hoo **boy** [link](/link)')
       visit "/recently_updated"
-      page.slug.must have_text "index"
+      page.must have_text "Updated Page"
     end
   end
 end
